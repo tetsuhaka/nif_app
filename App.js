fd4a334a1b27596b2fa06fa1a8212f1d5b70c306
Taiki Sekiguchi <ukurere3617@gmai.com>
2020-12-09 22:45:53 +0900

    modify app.js and add card.js and 2 modules

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+ //import * as React from 'react';
+import React, {useState} from 'react';
+import { Text,
+  View,
+  StyleSheet,
+  FlatList,
+  UIManager,
+  LayoutAnimation,
+  TouchableOpacity,
+  SafeAreaView,
+  Platform,
+  ImageBackground } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Card from './Card';
+
+if (Platform.OS === 'android') {
+  UIManager.setLayoutAnimationEnabledExperimental &&
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+}
 
 function HomeScreen() {
   return (
@@ -11,13 +27,110 @@ function HomeScreen() {
   );
 }
 
-function SettingsScreen() {
+function TimerScreen() {
+  const [dataSource, setDataSource] = useState([]);
+
+  const setAnimation = () => {
+    LayoutAnimation.configureNext({
+      duration: 250,
+      update: {
+        type: LayoutAnimation.Types.easeIn,
+        springDamping: 0.7,
+      },
+    });
+    LayoutAnimation.configureNext({
+      duration: 500,
+      create: {
+        type: LayoutAnimation.Types.easeIn,
+        property: LayoutAnimation.Properties.scaleXY,
+        springDamping: 0.7,
+      },
+    });
+  };
+
+  const addItem = (() => {
+    let key = dataSource.length;
+    return () => {
+      dataSource.unshift({
+        key,
+        animated: true,
+      });
+      setAnimation();
+      setDataSource(dataSource.slice(0));
+      console.log(dataSource);
+      console.log(key);
+      key++;
+    };
+  })();
+
+  const removeItem = (key) => {
+    setAnimation();
+    setDataSource(
+         dataSource.slice().filter((item) => item.key !== key)
+    );
+    console.log(key);
+  };
+
+  const ItemView = ({item}) => {
+    return (
+      // Flat List Item
+      <Card item={item} removeItem={removeItem} />
+    );
+  };
+
+  const ItemSeparatorView = () => {
+    return (
+      // Flat List Item Separator
+      <View/>
+    );
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
+    <SafeAreaView style={{
+      flex: 1,
+      backgroundColor: 'rgb(245,225,215)'
+    }}>
+      <ImageBackground source = {require('./img/timer.png')} style = {{width:'100%',height:'100%' }}>
+            <View style ={{width:'100%',height:'10%',backgroundColor: 'hsla(0,0%,0%,0)'}}></View>
+      <TouchableOpacity
+        style={styles.addButtonStyle}
+        onPress={addItem}>
+        <Text style={styles.addIconStyle}>
+            +
+        </Text>
+      </TouchableOpacity>
+      <FlatList
+        data={dataSource}
+        keyExtractor={(item, index) => index.toString()}
+        ItemSeparatorComponent={ItemSeparatorView}
+        renderItem={ItemView}
+      />
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  addButtonStyle: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    elevation: 3,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 15,
+    position: "absolute",
+    top: '3%',
+    right: '4%'
+  },
+  addIconStyle: {
+    color: 'white',
+    padding: 10,
+    fontSize: 40,
+    textAlign: 'center',
+  },
+});
 
 const Tab = createBottomTabNavigator();
 
@@ -26,7 +139,7 @@ export default function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Settings" component={TimerScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
