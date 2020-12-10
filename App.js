b1c8d3200342a00ab1f4d52f4895f332c8df661a
tetsuhaka <lime522tetsu@gmail.com>
2020-12-10 11:09:20 +0900

    [Add] Test.js

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
-import * as React from 'react';
+import React, {Component} from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Test from './Test';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
+// function HomeScreen() {
+//   return (
+//     <Test/>
+//   );
+// }
 
 function SettingsScreen() {
   return (
@@ -25,7 +24,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Home" component={Test} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
